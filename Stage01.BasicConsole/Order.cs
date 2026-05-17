241d4565b24297adbe5e1eb08cf625e9cfaa196e
mt khademi <Mohande3khademi@gmail.com>
2026-05-17 09:01:23 -0700

    feat: stage01 - write a basic console app

---
+++ Stage01.BasicConsole/Order.cs
@@ -0,0 +1,34 @@
+﻿namespace Stage01.BasicConsole;
+
+internal class Order
+{
+    public int Id { get; set; }
+    public List<OrderItem> Items { get; set; } = new();
+    public DateTime OrderDate { get; set; } = DateTime.Now;
+    public OrderStatus Status { get; set; } = OrderStatus.Pending;
+
+
+    public decimal TotalPrice => Items.Sum(item => item.TotalPrice);
+
+    public override string ToString()
+        => $"Id : {Id} | {OrderDate:yy-mm-dd hh:mm:ss} | items : {Items.Count} | {TotalPrice:0000.00}";
+
+}
+
+public class OrderItem
+{
+    public int ProductId { get; set; }
+    public int Quantity { get; set; }
+    public decimal UnitPrice { get; set; }
+
+    public decimal TotalPrice => Quantity * UnitPrice;
+}
+
+public enum OrderStatus
+{
+    Pending,
+    Confirmed,
+    Shipped,
+    Delivered,
+    Cancelled
+}
---
+++ Stage01.BasicConsole/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Stage01.BasicConsole;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public int StockQuantity { get; set; }
+
+    public override string ToString()
+        => $"Id: {Id} | {Name} | Price: {Price} | Stock Quantity: {StockQuantity}";
+}
