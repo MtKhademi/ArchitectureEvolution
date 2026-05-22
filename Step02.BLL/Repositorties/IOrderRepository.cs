1711b6b64fc4ca9894e6bd456a13b937f379eb94
mt khademi <Mohande3khademi@gmail.com>
2026-05-22 05:19:24 -0700

    ste05 add IOrderService

---
+++ Step02.BLL/Repositorties/IOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using Step02.BLL.Entities;
+
+namespace Step02.BLL.Repositorties;
+
+public interface IOrderRepository
+{
+    void Add(Order order);
+    void Update(Order order);
+    Order GetById(int id);
+    List<Order> GetAll();
+    List<Order> GetByStatus(OrderStatus status);
+}
---
+++ Step02.DAL/Repositories/OrderRepository.cs
@@ -0,0 +1,48 @@
+﻿using Step02.BLL.Entities;
+using Step02.BLL.Repositorties;
+using Step02.DAL.Data;
+
+namespace Step02.DAL.Repositories;
+
+public class OrderRepository : IOrderRepository
+{
+    private readonly DatabaseContext _db;
+
+    public OrderRepository(DatabaseContext db)
+    {
+        _db = db;
+    }
+
+    public void Add(Order order)
+    {
+        order.Id = DatabaseContext.OrderNextId++;
+        _db.Orders.Add(order);
+    }
+
+    public void Update(Order order)
+    {
+        var index = _db.Orders.FindIndex(o => o.Id == order.Id);
+        if (index != -1)
+            _db.Orders[index] = order;
+    }
+
+    public Order GetById(int id)
+    {
+        return _db.Orders.FirstOrDefault(o => o.Id == id);
+    }
+
+    public List<Order> GetAll()
+    {
+        return _db.Orders
+            .OrderByDescending(o => o.OrderDate)
+            .ToList();
+    }
+
+    public List<Order> GetByStatus(OrderStatus status)
+    {
+        return _db.Orders
+            .Where(o => o.Status == status)
+            .OrderByDescending(o => o.OrderDate)
+            .ToList();
+    }
+}
