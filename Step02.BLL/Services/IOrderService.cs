73057e62e6c33ae4ad75d6e1da9ae51b670fd606
mt khademi <Mohande3khademi@gmail.com>
2026-05-22 05:19:24 -0700

    ste05 add IOrderService

---
+++ Step02.BLL/Services/IOrderService.cs
@@ -0,0 +1,143 @@
+﻿using Step02.BLL.Entities;
+using Step02.BLL.Repositorties;
+using System.Threading.Channels;
+
+namespace Step02.BLL.Services;
+
+
+//قوانین کسب‌و‌کار:
+
+//❌ وقتی Delivered یا Shipped شد، دیگه نمی‌شه Cancel کرد
+//❌ سفارش Cancelled رو نمی‌شه دوباره فعال کرد
+//❌ نمی‌شه از Pending مستقیم به Delivered رفت(باید ترتیب طی بشه)
+//✅ فقط Pending و Confirmed قابل کنسل شدن هستنAIN
+//✅ موقع Confirm باید دوباره موجودی چک بشه(ممکنه موجودی کم شده باشه)
+
+public interface IOrderService
+{
+    Order CreateOrder(List<int> productIds, List<int> quantities);
+    Order GetOrderById(int orderId);
+    List<Order> GetAllOrders();
+    List<Order> GetOrdersByStatus(OrderStatus status);
+    Order UpdateOrderStatus(int orderId, OrderStatus newStatus);
+    Order CancelOrder(int orderId);
+}
+
+public class OrderService : IOrderService
+{
+    private readonly IOrderRepository _orderRepository;
+    private readonly IProductRepository _productRepository;
+
+    public OrderService(IOrderRepository orderRepository, IProductRepository productRepository)
+    {
+        _orderRepository = orderRepository;
+        _productRepository = productRepository;
+    }
+
+
+
+    public Order CancelOrder(int orderId)
+    {
+        return UpdateOrderStatus(orderId, OrderStatus.Cancelled);
+    }
+
+    public Order CreateOrder(List<int> productIds, List<int> quantities)
+    {
+        //-> Validation
+        if (productIds == null || productIds.Count == 0) throw new ArgumentException("Order must contain at least one product.");
+        if (quantities == null || quantities.Count != productIds.Count) throw new ArgumentException("Product count and quantity count must match.");
+
+
+        var orderItems = new List<OrderItem>();
+        for (int i = 0; i < productIds.Count; i++)
+        {
+            var productId = productIds[i];
+            var quantity = quantities[i];
+
+            if (quantity <= 0) throw new ArgumentException($"Invalid quantity ({quantity}) for product ID {productId}.");
+
+            var product = _productRepository.GetById(productId);
+
+            //-> Validation Base Count quantity
+            if (product.StockQuantity < quantity)
+                throw new InvalidOperationException(
+                    $"Not enough stock for '{productId}:{product.Name}'. " +
+                    $"Available: {product.StockQuantity}, Requested: {quantity}");
+
+
+            orderItems.Add(new OrderItem
+            {
+                ProductId = productId,
+                Quantity = quantity,
+                UnitPrice = product.Price
+            });
+
+            product.StockQuantity -= quantity;
+            _productRepository.Update(product);
+        }
+
+        var orderEntity = new Order()
+        {
+            Items = orderItems
+        };
+        _orderRepository.Add(orderEntity);
+        return orderEntity;
+
+    }
+
+    public List<Order> GetAllOrders() => _orderRepository.GetAll();
+
+    public Order GetOrderById(int orderId)
+    {
+        var order = _orderRepository.GetById(orderId);
+        if (order == null)
+            throw new KeyNotFoundException($"Order with ID {orderId} not found.");
+
+        return order;
+    }
+
+    public List<Order> GetOrdersByStatus(OrderStatus status) => _orderRepository.GetByStatus(status);
+
+    public Order UpdateOrderStatus(int orderId, OrderStatus newStatus)
+    {
+        var orderEntity = GetOrderById(orderId);
+
+        //-> Valiodation
+        if (newStatus == OrderStatus.Cancelled && (orderEntity.Status == OrderStatus.Shipped || orderEntity.Status == OrderStatus.Delivered))
+        {
+            throw new InvalidOperationException($"Order is already : {orderEntity.Status}, can not cancelled.");
+        }
+
+        if (newStatus == OrderStatus.Cancelled && (orderEntity.Status == OrderStatus.Pending || orderEntity.Status == OrderStatus.Confirmed))
+        {
+            throw new InvalidOperationException($"Order is already : {orderEntity.Status}, can not cancelled.");
+        }
+
+        ValidationStatusTransaction(orderEntity.Status, newStatus);
+
+        orderEntity.Status = newStatus;
+        _orderRepository.Update(orderEntity);
+
+        return orderEntity;
+    }
+
+    private void ValidationStatusTransaction(OrderStatus currentStatus, OrderStatus newStatus)
+    {
+        var allowedTransactions = new Dictionary<OrderStatus, List<OrderStatus>>
+        {
+            { OrderStatus.Pending, new List<OrderStatus> { OrderStatus.Confirmed, OrderStatus.Cancelled } },
+            { OrderStatus.Confirmed, new List<OrderStatus> { OrderStatus.Shipped, OrderStatus.Cancelled } },
+            { OrderStatus.Shipped, new List<OrderStatus> { OrderStatus.Delivered } }
+        };
+
+
+        if (!allowedTransactions.ContainsKey(currentStatus))
+            throw new InvalidOperationException($"Unknown order status: {currentStatus}");
+
+        if (!allowedTransactions[currentStatus].Contains(newStatus))
+            throw new InvalidOperationException(
+                $"Cannot change order status from '{currentStatus}' to '{newStatus}'. " +
+                $"Allowed: {string.Join(", ", allowedTransactions[currentStatus])}");
+
+    }
+}
