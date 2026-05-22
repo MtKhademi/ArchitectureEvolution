42dd97a56a07cdb70e6a79b57f177366d14997bf
mt khademi <Mohande3khademi@gmail.com>
2026-05-22 06:06:17 -0700

    Step013: Add user management

--- Step02.DAL/Data/DatabaseContext.cs
+++ Step02.DAL/Data/DatabaseContext.cs
@@ -6,8 +6,10 @@ public class DatabaseContext
 {
     public List<Product> Products = new List<Product>();
     public List<Order> Orders = new List<Order>();
+    public List<User> Users = new List<User>();
 
 
     public static int ProductNextId = 1;
+    public static int UserNextId = 1;
     public static int OrderNextId = 1;
 }
